697a49423f166b065f7229f836ba840331fe61bc
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2020-05-20 08:06:54 -0600

    Add file upload feature

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
 import pandas as pd
+import os
+from werkzeug.utils import secure_filename
 
 @app.route("/")
 def index():
@@ -50,3 +52,49 @@ def jinja():
         colors = colors, friends = friends,
         GitRemote = GitRemote, repeat = repeat,
         data = df.to_html(classes="table table-hover table-striped table-sm"))
+
+app.config["FILE_UPLOADS"] = "E:/Google Drive/git/flask-app-sample/data"
+app.config["ALLOWED_IMAGE_EXTENTIONS"] = ["CSV"]
+app.config["MAX_FILE_SIZE"] = 0.5 * 1024 * 1024
+
+def allowed_file(file_name):
+    if not "." in file_name:
+        return False
+    ext = file_name.rsplit(".",1)[1]
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENTIONS"]:
+        return True
+    else:
+        return False
+
+def allowed_file_size(filesize):
+    if int(filesize) <= app.config['MAX_FILE_SIZE']:
+        return True
+    else:
+        return False
+
+@app.route("/upload-file", methods=["POST", "GET"])
+def upload_file():
+
+    if request.method == "POST":
+        if request.files:
+            if not allowed_file_size(request.cookies.get("filesize")):
+                print("File exceeded max file size")
+                return redirect(request.url)
+
+            print(request.cookies)
+            f = request.files["file"]
+            if f.filename == "":
+                print("File must have a file name.")
+                return redirect(request.url)
+
+            if not allowed_file(f.filename):
+                print("file extention not allowed")
+                return redirect(request.url)
+            else:
+                filename = secure_filename(f.filename)
+                f.save(os.path.join(app.config["FILE_UPLOADS"], filename))
+                print("Image saved")
+
+            return redirect(request.url)
+
+    return render_template("/public/upload-file.html")
